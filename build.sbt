e2d97fe459c7d56997b6b2c630ead1b418b9e38b
Ben Booth <benwbooth@gmail.com>
2017-08-04 17:01:27 -0700

    WIP

--- build.sbt
+++ build.sbt
@@ -1,3 +1,4 @@
+import java.util
 import java.util.regex.Pattern
 
 import sbt._
@@ -27,7 +28,9 @@ lazy val figtools = (project in file(".")).
       "net.imagej" % "ij" % "1.50i",
       "net.sourceforge.tess4j" % "tess4j" % "3.4.0",
       "edu.stanford.nlp" % "stanford-corenlp" % "3.8.0",
-      "org.json4s" % "json4s-jackson_2.12" % "3.5.3"
+      "edu.stanford.nlp" % "stanford-corenlp" % "3.8.0" classifier "models-english",
+      "org.json4s" % "json4s-jackson_2.12" % "3.5.3",
+      "com.github.pathikrit" % "better-files_2.12" % "3.0.0"
     ),
     //logLevel in assembly := Level.Debug,
     assemblyOutputPath in assembly := baseDirectory.value / "bin" / name.value,
@@ -39,10 +42,19 @@ lazy val figtools = (project in file(".")).
     assemblyOption in assembly := (assemblyOption in assembly).value.copy(prependShellScript = {
       val coursier = (baseDirectory.value / "lib" / "coursier.jar").toString
       val artifacts = (libraryDependencies.value).
-        mkString(" ")
-      val jars = Process(s"java -cp $coursier coursier.Bootstrap fetch $artifacts").lines_!.
+        map(x => {
+          val parts: Array[String] = x.explicitArtifacts.map(x=>x.)
+          val artfact = parts.take(3).mkString(":")
+          val classifier = parts.drop(3).mkString(":")
+          s"$artifact${if (classifier.isEmpty) "" else s" -C $classifier"}"
+        }).mkString(" ")
+
+      val cmd = s"java -cp $coursier coursier.Bootstrap fetch $artifacts"
+      println(s"running cmd: $cmd")
+      val jars = Process(cmd).lines_!.
         map(x => x.replaceFirst(s"""^${Pattern.quote(System.getProperty("user.home"))}/(.*)$$""","""\$HOME/$1""")).
         mkString(" ")
+
       val repos = resolvers.value.map(
         _.toString.replaceFirst("""^[^:]*:\s*""","").split(" ").
           map(x=>s"-r $x").mkString(" ")).mkString(" ")
--- src/figtools/FigTools.scala
+++ src/figtools/FigTools.scala
@@ -1,7 +1,79 @@
 package figtools
+import java.util.Properties
+
+import better.files._
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation
+import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
+import edu.stanford.nlp.trees.Tree
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+import scopt.OptionParser
+
+import collection.JavaConverters._
+import edu.stanford.nlp.util.logging.RedwoodConfiguration
 
 object FigTools {
+  implicit val formats = org.json4s.DefaultFormats
+
+  case class Config(mode: String = "",
+                    pdfExportResolution: Int = 150)
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val parser = new OptionParser[Config]("figtools") {
+      override def showUsageOnError = true
+
+      head("figtools", "0.1.0")
+
+      help("help").text("prints this usage text")
+      version("version").text("print the program version")
+
+      cmd("analyze").action((_, c) => c.copy(mode = "analyze")).
+        text("recursively analyze a nested directory of figures.").
+        children(
+          opt[Int]("pdf-export-resolution").action((x,c) => c.copy(pdfExportResolution = x)).
+            text("Resolution to use when exporting PDFs to images")
+        )
+    }
+    parser.parse(args, Config()) match {
+      case Some(config) =>
+        config.mode match {
+          case "analyze" => analyze(config)
+          case "" => parser.showUsage()
+        }
+      case None =>
+        System.exit(1)
+    }
+  }
+
+  def treeToJSON(tree: Tree): JValue = {
+    JArray(List(JString(tree.label().toString()))) ++ (
+      if (tree.isLeaf) JArray(List(JString(tree.value()))) else treeToJSON(tree))
+  }
+
+  def analyze(config: Config): Unit = {
+    // turn off CoreNLP logging
+    RedwoodConfiguration.current.clear.apply()
+    val dir = "." / "."
+    for (datapackage <- dir.listRecursively.filter(_.name == "datapackage.json")) {
+      val json = parse(datapackage.contentAsString)
+      val description_nohtml = (json \ "description_nohtml").extract[String]
+      println(s"file=$datapackage")
+      println(s"description_nohtml=$description_nohtml")
+
+      val props = new Properties()
+      props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref")
+      val pipeline = new StanfordCoreNLP(props)
+      val document = new Annotation(description_nohtml)
+      pipeline.annotate(document)
+      val sentences = document.get(classOf[SentencesAnnotation])
+
+      for (sentence <- sentences.asScala) {
+        val tree = sentence.get(classOf[TreeAnnotation])
+        val json = treeToJSON(tree)
+        println(s"sentence=${sentence.toString}")
+        println(s"tree=${pretty(render(json))}")
+      }
+    }
   }
 }

cdd9f29c05c026614145872eab89f55bb26c3120
Ben Booth <benwbooth@gmail.com>
2018-10-11 12:36:15 -0700

    fix test

--- test/src/figtools/AnalyzeSuite.scala
+++ test/src/figtools/AnalyzeSuite.scala
@@ -72,41 +72,27 @@ class AnalyzeSuite extends FunSuite {
       new AnalyzeImage(dir=TestDir, ids=Seq("3872106"), url=Some(Url)).analyze(),
       Map(
         "3872106" -> Map(
-          "Fig_3.tif" -> Map(
+          "Fig 4.TIF" -> Map(
             "a" -> LabelResult(
-              ArrayBuffer(
-                "(a): \u03b3ECS activity in the roots; "
-              ),
+              ArrayBuffer("(a): \u03b3ECS activity in the roots; "),
               ArrayBuffer(Box(17, 10, 2177, 1262))
             ),
             "b" -> LabelResult(
-              ArrayBuffer(
-                "(b): GSHS activity in the roots; "
-              ),
-              ArrayBuffer(
-                Box(11, 1261, 2171, 2548)
-              )
+              ArrayBuffer("(b): GSHS activity in the roots; "),
+              ArrayBuffer(Box(11, 1261, 2171, 2548))
             ),
             "c" -> LabelResult(
-              ArrayBuffer(
-                "(c): \u03b3ECS activity in the leaves; "
-              ),
-              ArrayBuffer(
-                Box(2178, 10, 4290, 1260)
-              )
+              ArrayBuffer("(c): \u03b3ECS activity in the leaves; "),
+              ArrayBuffer(Box(2178, 10, 4290, 1260))
             ),
             "d" -> LabelResult(
-              ArrayBuffer(
-                "(d): GSHS activity in the leaves. (For details see legend of Table 1)."
-              ),
-              ArrayBuffer(
-                Box(2172, 1261, 4290, 2548)
-              )
+              ArrayBuffer("(d): GSHS activity in the leaves. (For details see legend of Table 1)."),
+              ArrayBuffer(Box(2172, 1261, 4290, 2548))
             )
           )
         )
-      )))
+      )
+    ))
   }
-
 
 }
